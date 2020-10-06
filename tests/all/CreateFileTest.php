d76004c5597ea414ef74394a74ea2db2dabbb1ae
Anton Smirnov <sandfox@sandfox.me>
2020-10-06 19:10:29 +0300

    Check infohash instead of pieces

--- tests/all/CreateFileTest.php
+++ tests/all/CreateFileTest.php
@@ -13,20 +13,16 @@ class CreateFileTest extends TestCase
     {
         $torrentFile = TorrentFile::fromPath(TEST_ROOT . '/data/files/file1.txt'); // approx 6 mb
 
-        $piecesHashes = $torrentFile->getRawData()['info']['pieces'];
-
-        $this->assertEquals('6f714b23b94595c694357083492c363c', md5($piecesHashes));
-        $this->assertEquals(260, strlen($piecesHashes)); // 13 chunks
+        $this->assertEquals('6ca8fecb8d4c43183307179652fd31a50f99a912', $torrentFile->getInfoHash());
+        $this->assertEquals(260, strlen($torrentFile->getRawData()['info']['pieces'])); // 13 chunks
     }
 
     public function testMultipleFiles(): void
     {
         $torrentFile = TorrentFile::fromPath(TEST_ROOT . '/data/files'); // approx 19 mb
 
-        $piecesHashes = $torrentFile->getRawData()['info']['pieces'];
-
-        $this->assertEquals('9cdfab1d0a4423dc2d2fd5533d374e4f', md5($piecesHashes));
-        $this->assertEquals(760, strlen($piecesHashes)); // 38 chunks
+        $this->assertEquals('2efb80c60b42b261d79b777477276e0b18b47081', $torrentFile->getInfoHash());
+        $this->assertEquals(760, strlen($torrentFile->getRawData()['info']['pieces'])); // 38 chunks
     }
 
     public function testMultipleFiles1MB(): void
@@ -35,9 +31,7 @@ class CreateFileTest extends TestCase
             'pieceLength' => 1024 * 1024, // 1mb chunk
         ]); // approx 19 mb
 
-        $piecesHashes = $torrentFile->getRawData()['info']['pieces'];
-
-        $this->assertEquals('428ff41d4db721e925ef0e022eaa6cdf', md5($piecesHashes));
-        $this->assertEquals(380, strlen($piecesHashes)); // 19 chunks
+        $this->assertEquals('5af63cdfb9cdcc9a09bde3fa4b7a9266d8528b7a', $torrentFile->getInfoHash());
+        $this->assertEquals(380, strlen($torrentFile->getRawData()['info']['pieces'])); // 19 chunks
     }
 }
