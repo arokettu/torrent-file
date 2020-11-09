d615d02ee6410a96a0f3a11fdd7d9cf29e17c85b
Anton Smirnov <sandfox@sandfox.me>
2020-11-09 03:17:46 +0200

    Update file checks

--- tests/all/CreateFileTest.php
+++ tests/all/CreateFileTest.php
@@ -13,27 +13,48 @@ class CreateFileTest extends TestCase
 {
     public function testSingleFile(): void
     {
-        $torrentFile = TorrentFile::fromPath(TEST_ROOT . '/data/files/file1.txt'); // approx 6 mb
+        $torrent = TorrentFile::fromPath(TEST_ROOT . '/data/files/file1.txt'); // approx 6 mb
 
-        $this->assertEquals('6ca8fecb8d4c43183307179652fd31a50f99a912', $torrentFile->getInfoHash());
-        $this->assertEquals(260, strlen($torrentFile->getRawData()['info']['pieces'])); // 13 chunks
+        $this->assertEquals('6ca8fecb8d4c43183307179652fd31a50f99a912', $torrent->getInfoHash());
+        $this->assertEquals(260, strlen($torrent->getRawData()['info']['pieces'])); // 13 chunks
+        $this->assertEquals('file1.txt', $torrent->getDisplayName());
+        $this->assertEquals('file1.txt.torrent', $torrent->getFileName());
+
+        $this->assertEquals(
+            'magnet:?dn=file1.txt&xt=urn:btih:6CA8FECB8D4C43183307179652FD31A50F99A912',
+            $torrent->getMagnetLink()
+        );
     }
 
     public function testMultipleFiles(): void
     {
-        $torrentFile = TorrentFile::fromPath(TEST_ROOT . '/data/files'); // approx 19 mb
+        $torrent = TorrentFile::fromPath(TEST_ROOT . '/data/files'); // approx 19 mb
 
-        $this->assertEquals('2efb80c60b42b261d79b777477276e0b18b47081', $torrentFile->getInfoHash());
-        $this->assertEquals(760, strlen($torrentFile->getRawData()['info']['pieces'])); // 38 chunks
+        $this->assertEquals('2efb80c60b42b261d79b777477276e0b18b47081', $torrent->getInfoHash());
+        $this->assertEquals(760, strlen($torrent->getRawData()['info']['pieces'])); // 38 chunks
+        $this->assertEquals('files', $torrent->getDisplayName());
+        $this->assertEquals('files.torrent', $torrent->getFileName());
+
+        $this->assertEquals(
+            'magnet:?dn=files&xt=urn:btih:2EFB80C60B42B261D79B777477276E0B18B47081',
+            $torrent->getMagnetLink()
+        );
     }
 
     public function testMultipleFiles1MB(): void
     {
-        $torrentFile = TorrentFile::fromPath(TEST_ROOT . '/data/files', [
+        $torrent = TorrentFile::fromPath(TEST_ROOT . '/data/files', [
             'pieceLength' => 1024 * 1024, // 1mb chunk
         ]); // approx 19 mb
 
-        $this->assertEquals('5af63cdfb9cdcc9a09bde3fa4b7a9266d8528b7a', $torrentFile->getInfoHash());
-        $this->assertEquals(380, strlen($torrentFile->getRawData()['info']['pieces'])); // 19 chunks
+        $this->assertEquals('5af63cdfb9cdcc9a09bde3fa4b7a9266d8528b7a', $torrent->getInfoHash());
+        $this->assertEquals(380, strlen($torrent->getRawData()['info']['pieces'])); // 19 chunks
+        $this->assertEquals('files', $torrent->getDisplayName());
+        $this->assertEquals('files.torrent', $torrent->getFileName());
+
+        $this->assertEquals(
+            'magnet:?dn=files&xt=urn:btih:5AF63CDFB9CDCC9A09BDE3FA4B7A9266D8528B7A',
+            $torrent->getMagnetLink()
+        );
     }
 }
--- tests/all/LoadFileTest.php
+++ tests/all/LoadFileTest.php
@@ -25,5 +25,27 @@ class LoadFileTest extends TestCase
         $this->assertEquals(1481207147, $torrent->getCreationDate());
         $this->assertFalse($torrent->isPrivate());
         $this->assertEquals('54259d2fafb1de5b794e449777748eba36236f8c', $torrent->getInfoHash());
+        $this->assertEquals('CentOS-7-x86_64-NetInstall-1611', $torrent->getDisplayName());
+        $this->assertEquals('CentOS-7-x86_64-NetInstall-1611.torrent', $torrent->getFileName());
+
+        // magnet link
+
+        $this->assertEquals(
+            'magnet:?dn=CentOS-7-x86_64-NetInstall-1611&xt=urn:btih:54259D2FAFB1DE5B794E449777748EBA36236F8C' .
+                '&tr=http%3A%2F%2Ftorrent.centos.org%3A6969%2Fannounce' .
+                '&tr=http%3A%2F%2Fipv6.torrent.centos.org%3A6969%2Fannounce',
+            $torrent->getMagnetLink()
+        );
+
+        // reencoded file should be exactly same
+
+        $tmpfile = tmpfile();
+        fwrite($tmpfile, $torrent->storeToString());
+        fflush($tmpfile);
+
+        $this->assertFileEquals(
+            TEST_ROOT . '/data/CentOS-7-x86_64-NetInstall-1611.torrent',
+            stream_get_meta_data($tmpfile)['uri']
+        );
     }
 }
