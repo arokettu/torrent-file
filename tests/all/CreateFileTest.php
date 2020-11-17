af034ebde5df07046e1ba7666a5dab928133ff30
Anton Smirnov <sandfox@sandfox.me>
2020-11-17 22:04:48 +0200

    Include md5 into test

--- tests/all/CreateFileTest.php
+++ tests/all/CreateFileTest.php
@@ -14,30 +14,34 @@ class CreateFileTest extends TestCase
 {
     public function testSingleFile(): void
     {
-        $torrent = TorrentFile::fromPath(TEST_ROOT . '/data/files/file1.txt'); // approx 6 mb
+        $torrent = TorrentFile::fromPath(TEST_ROOT . '/data/files/file1.txt', [
+            'md5sum' => true,
+        ]); // approx 6 mb
 
-        $this->assertEquals('6ca8fecb8d4c43183307179652fd31a50f99a912', $torrent->getInfoHash());
+        $this->assertEquals('6092cfe0e10d639229cdb76a1375af37e45c0df7', $torrent->getInfoHash());
         $this->assertEquals(260, strlen($torrent->getRawData()['info']['pieces'])); // 13 chunks
         $this->assertEquals('file1.txt', $torrent->getDisplayName());
         $this->assertEquals('file1.txt.torrent', $torrent->getFileName());
 
         $this->assertEquals(
-            'magnet:?dn=file1.txt&xt=urn:btih:6CA8FECB8D4C43183307179652FD31A50F99A912',
+            'magnet:?dn=file1.txt&xt=urn:btih:6092CFE0E10D639229CDB76A1375AF37E45C0DF7',
             $torrent->getMagnetLink()
         );
     }
 
     public function testMultipleFiles(): void
     {
-        $torrent = TorrentFile::fromPath(TEST_ROOT . '/data/files'); // approx 19 mb
+        $torrent = TorrentFile::fromPath(TEST_ROOT . '/data/files', [
+            'md5sum' => true,
+        ]); // approx 19 mb
 
-        $this->assertEquals('2efb80c60b42b261d79b777477276e0b18b47081', $torrent->getInfoHash());
+        $this->assertEquals('0c8af23beb533d29fe210137439e6c1fce8acaba', $torrent->getInfoHash());
         $this->assertEquals(760, strlen($torrent->getRawData()['info']['pieces'])); // 38 chunks
         $this->assertEquals('files', $torrent->getDisplayName());
         $this->assertEquals('files.torrent', $torrent->getFileName());
 
         $this->assertEquals(
-            'magnet:?dn=files&xt=urn:btih:2EFB80C60B42B261D79B777477276E0B18B47081',
+            'magnet:?dn=files&xt=urn:btih:0C8AF23BEB533D29FE210137439E6C1FCE8ACABA',
             $torrent->getMagnetLink()
         );
     }
@@ -45,16 +49,17 @@ class CreateFileTest extends TestCase
     public function testMultipleFiles1MB(): void
     {
         $torrent = TorrentFile::fromPath(TEST_ROOT . '/data/files', [
+            'md5sum' => true,
             'pieceLength' => 1024 * 1024, // 1mb chunk
         ]); // approx 19 mb
 
-        $this->assertEquals('5af63cdfb9cdcc9a09bde3fa4b7a9266d8528b7a', $torrent->getInfoHash());
+        $this->assertEquals('7f71b004d89e823b7800e9f27c893c3a97562cea', $torrent->getInfoHash());
         $this->assertEquals(380, strlen($torrent->getRawData()['info']['pieces'])); // 19 chunks
         $this->assertEquals('files', $torrent->getDisplayName());
         $this->assertEquals('files.torrent', $torrent->getFileName());
 
         $this->assertEquals(
-            'magnet:?dn=files&xt=urn:btih:5AF63CDFB9CDCC9A09BDE3FA4B7A9266D8528B7A',
+            'magnet:?dn=files&xt=urn:btih:7F71B004D89E823B7800E9F27C893C3A97562CEA',
             $torrent->getMagnetLink()
         );
     }
