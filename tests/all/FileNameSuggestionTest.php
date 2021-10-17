ecfceef837ec7a1c568b2b9ca0f0e2f999561798
Anton Smirnov <sandfox@sandfox.me>
2021-10-17 20:41:13 +0300

    Rename test to be more descriptive

--- tests/all/FileNameSuggestionTest.php
+++ tests/all/FileNameSuggestionTest.php
@@ -8,7 +8,7 @@ use PHPUnit\Framework\TestCase;
 use SandFox\Bencode\Bencode;
 use SandFox\Torrent\TorrentFile;
 
-class AuxFieldsTest extends TestCase
+class FileNameSuggestionTest extends TestCase
 {
     public function testDisplayName(): void
     {
