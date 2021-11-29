05e4417f0bafbd05241815b5f2d23d6098bde278
Anton Smirnov <sandfox@sandfox.me>
2021-11-29 16:06:25 +0200

    Test aligning

--- tests/files/CreateFileFeaturesTest.php
+++ tests/files/CreateFileFeaturesTest.php
@@ -194,4 +194,115 @@ class CreateFileFeaturesTest extends TestCase
             $info
         );
     }
+
+    public function testPadAlways(): void
+    {
+        $torrent = TorrentFile::fromPath(TEST_ROOT . '/data/files2', [
+            'pieceAlign' => true,
+        ]);
+
+        $info = $torrent->getRawData()['info'];
+
+//        echo export_test_data($info);
+        $this->assertEquals(
+            [
+                'files' => [
+                    [
+                        'length' => 6621359,
+                        'path' => ['dir1', 'file1.txt'],
+                        'sha1' => base64_decode("FLpF01Q+gHDBdrRmIDPqQmKaYgQ="),
+                    ],
+                    [
+                        'attr' => 'p',
+                        'length' => 194385,
+                        'path' => ['.pad', '194385'],
+                    ],
+                    [
+                        'length' => 6621359,
+                        'path' => ['dir2', 'file1.txt'],
+                        'sha1' => base64_decode("FLpF01Q+gHDBdrRmIDPqQmKaYgQ="),
+                    ],
+                    [
+                        'attr' => 'p',
+                        'length' => 194385,
+                        'path' => ['.pad', '194385'],
+                    ],
+                    [
+                        'length' => 6621341,
+                        'path' => ['dir2', 'file2.txt'],
+                        'sha1' => base64_decode("JToK2HdRS+5VKZCu8WhvbV9a9KY="),
+                    ],
+                    [
+                        'attr' => 'p',
+                        'length' => 194403,
+                        'path' => ['.pad', '194403'],
+                    ],
+                    [
+                        'length' => 6621341,
+                        'path' => ['dir3', 'file2.txt'],
+                        'sha1' => base64_decode("JToK2HdRS+5VKZCu8WhvbV9a9KY="),
+                    ],
+                    [
+                        'attr' => 'p',
+                        'length' => 194403,
+                        'path' => ['.pad', '194403'],
+                    ],
+                    [
+                        // no padding after when file is already aligned
+                        'length' => 6291456,
+                        'path' => ['dir4', 'aligned.txt'],
+                        'sha1' => base64_decode("8uHdutKp152UxbBUEr66/UNo/I0="),
+                    ],
+                    [
+                        'length' => 6621335,
+                        'path' => ['dir5', 'file3.txt'],
+                        'sha1' => base64_decode("WW5Dv31hzse3rO95vQfVTk7M3lg="),
+                    ],
+                    [
+                        'attr' => 'p',
+                        'length' => 194409,
+                        'path' => ['.pad', '194409'],
+                    ],
+                    [
+                        'attr' => 'x',
+                        'length' => 6621355,
+                        'path' => ['dir6', 'exec.txt'],
+                        'sha1' => base64_decode("PLesPfBgCmcfBdyu9k95eUh8sfs="),
+                    ],
+                ],
+                'name' => 'files2',
+                'piece length' => 524288,
+                'pieces' => base64_decode(<<<PIECES
+                    UA6+qBSqwP7uJvTrqHs5iSp5mUcYJfIZ0wAyzY2UHsZoDGPTMYeNeHBiUmrKwus8K15+gprxhB4ZmcoA/4vOAEQncUHA
+                    AkG2ApyqUloDAZ8XO3ktOMTUiQudWYbF+C7vrrYcJZZSA1ah8mNroUK9GEhJ/3tU40U4gfAgqRjk+AYay689QDM/8hpi
+                    YYegLmNYntD0erSEXD7G9Fy4DT1SOMM4lHtUQsC+7erlN+apGisf4erLaK2bGTgKsbDwETNk115guP75OsxO499nbjEf
+                    7uzNnu+SVo3wmeoI5/mx1jV2iihYK4Ow/iJL7yq2CUruA02xqlYBnQZYFR1EwRjpGzgLuklQDr6oFKrA/u4m9OuoezmJ
+                    KnmZRxgl8hnTADLNjZQexmgMY9Mxh414cGJSasrC6zwrXn6CmvGEHhmZygD/i84ARCdxQcACQbYCnKpSWgMBnxc7eS04
+                    xNSJC51ZhsX4Lu+uthwlllIDVqHyY2uhQr0YSEn/e1TjRTiB8CCpGOT4BhrLrz1AMz/yGmJhh6AuY1ie0PR6tIRcPsb0
+                    XLgNPVI4wziUe1RCwL7t6uU35qkaKx/h6storZsZOAqxsPARM2TXXmC4/vk6zE7j32duMR/u7M2e75JWjfCZ6gjn+bHW
+                    NXaKKFgrg7D+IkvvKrYJSu4DTbGqVgGdBlgVHUTBGOkbOAu6SfGd0UCYcHcCRmfE6oWkmfuP3hUQkT3cotC/ypkngzAL
+                    aNb8tSBh8o4s30NBRUecdo/mdf+aBwR/7smj594UsZf4Hk209atGipKi7k9fjkxPhHdwQQTyCA5ruZRqMKZLJmRkBn8M
+                    kDIJ3LiiGoANq2czAUh6kIttqJie+Hz5pcbDVpqoORTgucqpEebMODMSep65T5SEdxE6O7ICa3l8d0i1o/RjYorSVlTa
+                    RKfpUtP7IzI41Ql+lKty9nCoOz/RRKBaCamz7UbiXo7hSR8dCXlzdcNgfCBTVNZxdGwC+jsRFiZEbG5RCtVQXaLHXtb1
+                    AzRqk1+1gJoaQIn6bDvC2shabg4u8Z3RQJhwdwJGZ8TqhaSZ+4/eFRCRPdyi0L/KmSeDMAto1vy1IGHyjizfQ0FFR5x2
+                    j+Z1/5oHBH/uyaPn3hSxl/geTbT1q0aKkqLuT1+OTE+Ed3BBBPIIDmu5lGowpksmZGQGfwyQMgncuKIagA2rZzMBSHqQ
+                    i22omJ74fPmlxsNWmqg5FOC5yqkR5sw4MxJ6nrlPlIR3ETo7sgJreXx3SLWj9GNiitJWVNpEp+lS0/sjMjjVCX6Uq3L2
+                    cKg7P9FEoFoJqbPtRuJejuFJHx0JeXN1w2B8IFNU1nF0bAL6OxEWJkRsblEK1VBdosde1vUDNGqTX7WAmhpAifpsO8La
+                    yFpuDi5QDr6oFKrA/u4m9OuoezmJKnmZRxgl8hnTADLNjZQexmgMY9Mxh414cGJSasrC6zwrXn6CmvGEHhmZygD/i84A
+                    RCdxQcACQbYCnKpSWgMBnxc7eS04xNSJC51ZhsX4Lu+uthwlllIDVqHyY2uhQr0YSEn/e1TjRTiB8CCpGOT4BhrLrz1A
+                    Mz/yGmJhh6AuY1ie0PR6tIRcPsb0XLgNPVI4wziUe1RCwL7t6uU35qkaKx/h6storZsZOAqxsPARM2TXXmC4/vk6zE7j
+                    32duMR/u7M2e75JWjfCZ6gjn+bHWNXaKKFgrg7D+IkvvKrYJSu4o168liUzcR+xQkoQ+olUhU/wil1OrvHgj4Zx9Ygn6
+                    hdmmEdmBHVDxNfXgWs1CeCfECxNyINTe6ObshGAsJ1hIxUunzL9Sz2dPh6Lkb3Z5MI1mYuKf8WaMN3FAV1Yk9Gm9xmUf
+                    BuhItNqkf7Tl/z6n7laSudBf58mUZIFAHWRF0LwNWIX4lWaJtnHXY6JuTmRbEuxonF4ZkhoAozNAVoSqGW6eCj5Z7SJC
+                    itr+opamt7ZqMgU9nYoB0kx+5muwh0KyJDjmW0QqpsH+mZfDXwV0G5NyX7mnykr7Qw8Z2kb3Lb9OSibXFTjasGZK2akK
+                    JfFbCYZ0/DeekY26/ByqqNs8zRvkyE63QZojMIaSG2VcbzpPzbusLvpeojitOr2+/5su6UKaDqAyuD4FKE/pBrC/Ehk0
+                    UxjrU2mseOIy0egWBeDoj2kFSs/Op5EpAB8wlsk85T+xyvfA2LKjBvTScIKsSci+tlWvlOh6sDG/siLyrXWqodoBW0Xo
+                    4kv3wqRW86o3s5cbuRFM2JRi9t3Uk+q+sfJ8e9fmTl+x0Ycc0tm1brX8Y8bwmzYEfsg7qgAWau65ny/KtLrou4LaHIIk
+                    vH1gMVHrr2SN0C3PU90u/2g7vcwEcbVoloegxCRdcD+Bc8ERdWw5e8++0smCHqZ/7RP+67paBbIi6WBLebfa4EWqU8/T
+                    6Eihnq8O
+                    PIECES),
+            ],
+            $info
+        );
+    }
 }
