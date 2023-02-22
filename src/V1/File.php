2d60c5c7cd85d17408b1b3e72087f0f8e442cb68
Anton Smirnov <sandfox@sandfox.me>
2023-02-22 23:16:48 +0200

    Hex string for sha1

--- src/V1/File.php
+++ src/V1/File.php
@@ -11,15 +11,17 @@ final class File
 {
     public readonly array $path;
     public readonly ?array $symlinkPath;
+    public readonly ?string $sha1;
 
     public function __construct(
         ListObject $path,
         public readonly int $length,
         public readonly Attributes $attributes,
-        public readonly ?string $sha1,
+        public readonly ?string $sha1bin,
         ?ListObject $symlinkPath,
     ) {
         $this->path = $path->toArray();
         $this->symlinkPath = $symlinkPath?->toArray();
+        $this->sha1 = $sha1bin ? bin2hex($sha1bin) : null;
     }
 }
