98f75e4002a68d6e63cb04b4fae7f48f3ae9f1d2
Anton Smirnov <sandfox@sandfox.me>
2023-02-26 01:11:01 +0200

    InfoDict object

---
+++ src/DataTypes/Internal/InfoDict.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arokettu\Torrent\DataTypes\Internal;
+
+use Arokettu\Bencode\Encoder;
+
+/**
+ * @internal
+ */
+final class InfoDict
+{
+    public readonly string $infoString;
+
+    public function __construct(
+        private readonly DictObject $info
+    ) {
+        unset($this->infoString); // lazy creation
+    }
+
+    public function __get(string $name): mixed
+    {
+        return match ($name) {
+            'infoString' => $this->infoString = (new Encoder())->encode($this->info),
+        };
+    }
+}
